c36030354061a244e92ebf57eaf024e825dc8748
James <james.greygoose@gmail.com>
2020-04-21 18:05:44 +0100

    Update build folder

---
+++ build/precache-manifest.3405cde764b1f262517490d4012a13ba.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e461a107210475cebff8ac28641437a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83f57bba5a53eccc1c84",
+    "url": "/static/css/main.111122b6.chunk.css"
+  },
+  {
+    "revision": "ec0f8d67ae7376dc07fe",
+    "url": "/static/js/2.7c7ce28e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.7c7ce28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f57bba5a53eccc1c84",
+    "url": "/static/js/main.9acb9e9d.chunk.js"
+  },
+  {
+    "revision": "6f20c616e96314751e41",
+    "url": "/static/js/runtime-main.23ba303b.js"
+  },
+  {
+    "revision": "6f9a03bd4554e5454de1c79f4c91aadf",
+    "url": "/static/media/Linkedin.6f9a03bd.png"
+  },
+  {
+    "revision": "19b2f0ede7ffe3216c632b2f3966ca17",
+    "url": "/static/media/back-end.19b2f0ed.png"
+  },
+  {
+    "revision": "fe299061c7ec0dbbf92e57c310cb22e5",
+    "url": "/static/media/designer2.fe299061.png"
+  },
+  {
+    "revision": "1da0a7ead60006f6ba6ded7b37502b9a",
+    "url": "/static/media/front-end.1da0a7ea.png"
+  },
+  {
+    "revision": "578ff9f8ee536eae92f810af9eb28f33",
+    "url": "/static/media/problem.578ff9f8.png"
+  },
+  {
+    "revision": "881a5768e746fcf2a68840ed5579500a",
+    "url": "/static/media/screen.881a5768.png"
+  }
+]);
